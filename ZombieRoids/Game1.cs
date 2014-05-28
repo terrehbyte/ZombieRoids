c5d0e68eeebce7f5d90a6a90900422a4efe015ec
Terry Nguyen <terreh@terrehbyte.com>
2014-05-28 09:36:38 -0700

    Added input player movement

--- ZombieRoids/Game1.cs
+++ ZombieRoids/Game1.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.GamerServices;
 #endregion
@@ -19,6 +20,21 @@ namespace ZombieRoids
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Player player;
+
+        // Keyboard States
+        KeyboardState curKeyboardState;
+        KeyboardState prevKeyboardState;
+
+        // Gamepad States
+        GamePadState curGamepadState;
+        GamePadState prevGamepadState;
+
+        // Mouse States
+        MouseState curMouseState;
+        MouseState prevMouseState;
+
+        // Move Speed
+        float playerMoveSpeed;
 
         public Game1()
             : base()
@@ -43,6 +59,9 @@ namespace ZombieRoids
             player = new Player();
             Vector2 v2playerPos = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X,
                                               GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+
+            playerMoveSpeed = 8.0f;
+            TouchPanel.EnabledGestures = GestureType.FreeDrag;
 
             player.Initialize(Content.Load<Texture2D>("Graphics\\player"), v2playerPos);
         }
@@ -81,6 +100,51 @@ namespace ZombieRoids
             // TODO: Add your update logic here
 
             base.Update(gameTime);
+
+            prevGamepadState = curGamepadState;
+            prevKeyboardState = curKeyboardState;
+            prevMouseState = curMouseState;
+
+            curKeyboardState = Keyboard.GetState();
+            curGamepadState = GamePad.GetState(PlayerIndex.One);
+            curMouseState = Mouse.GetState();
+
+
+
+            UpdatePlayer(gameTime);
+        }
+
+        private void UpdatePlayer(GameTime gameTime)
+        {
+            // Gamepad Input
+            player.v2Pos.X += curGamepadState.ThumbSticks.Left.X * playerMoveSpeed;
+            player.v2Pos.Y += curGamepadState.ThumbSticks.Left.Y * playerMoveSpeed;
+
+            // KB || DPAD
+            if (curKeyboardState.IsKeyDown(Keys.Left) ||
+                curGamepadState.DPad.Left == ButtonState.Pressed)
+            {
+                player.v2Pos.X -= playerMoveSpeed;
+            }
+            if (curKeyboardState.IsKeyDown(Keys.Right) ||
+                curGamepadState.DPad.Right == ButtonState.Pressed)
+            {
+                player.v2Pos.X += playerMoveSpeed;
+            }
+            if (curKeyboardState.IsKeyDown(Keys.Up) ||
+                curGamepadState.DPad.Up == ButtonState.Pressed)
+            {
+                player.v2Pos.Y -= playerMoveSpeed;
+            }
+            if (curKeyboardState.IsKeyDown(Keys.Down) ||
+                curGamepadState.DPad.Down == ButtonState.Pressed)
+            {
+                player.v2Pos.Y += playerMoveSpeed;
+            }
+
+            player.v2Pos.X = MathHelper.Clamp(player.v2Pos.X, 0, GraphicsDevice.Viewport.Width - player.iWidth);
+            player.v2Pos.Y = MathHelper.Clamp(player.v2Pos.Y, 0, GraphicsDevice.Viewport.Height - player.iHeight);
+
         }
 
         /// <summary>
