c82e09000e154694d92fd982716aac88b5ea12e2
Terry Nguyen <terreh@terrehbyte.com>
2014-06-09 17:53:25 -0700

    Stubbed out StateStack and GameState

---
+++ ZombieRoids/GameState.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZombieRoids
+{
+    public abstract class GameState
+    {
+        /// <summary>
+        /// Loads the requested pieces of content to the game state
+        /// </summary>
+        protected virtual void LoadContent()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Unloads all content loaded by this game state
+        /// </summary>
+        protected virtual void UnloadContent()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Called to begin the game state
+        /// </summary>
+        public virtual void Start()
+        {
+            LoadContent();
+        }
+
+        /// <summary>
+        /// Called every game update frame
+        /// </summary>
+        public virtual void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Called to end the game state
+        /// </summary>
+        public virtual void End()
+        {
+            UnloadContent();
+        }
+    }
+}
---
+++ ZombieRoids/StateStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZombieRoids
+{
+    public static class StateStack
+    {
+        // Stack of GameStates
+        private static Stack<GameState> m_oStates = new Stack<GameState>();
+
+        /// <summary>
+        /// Pushes a new GameState to the top of the stack
+        /// </summary>
+        /// <param name="a_oState"></param>
+        public void AddState(GameState a_oState)
+        {
+            m_oStates.Push(a_oState);
+        }
+
+        /// <summary>
+        /// Pops the topmost GameState from the stack
+        /// </summary>
+        public void PopState()
+        {
+            m_oStates.Pop();
+        }
+
+        /// <summary>
+        /// Calls update on the topmost GameState
+        /// </summary>
+        public void Update()
+        {
+            m_oStates.Peek().Update();
+        }   
+    }
+}
